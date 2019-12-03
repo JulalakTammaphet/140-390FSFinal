a2bc403b42142101bddf1c1a70bdf17992cc8fe4
JulalakTammaphet <48485581+JulalakTammaphet@users.noreply.github.com>
2019-12-03 18:35:26 +0700

    Ans#6/#7/#8/#9

--- api.js
+++ api.js
@@ -19,29 +19,51 @@ function getAllProducts(req, res) {
 function getProductById(req, res) {
     var pid = req.params.pid;    
     // #6 Get a product by ID
-
+    Product.find({"_id":pid}, function (err, data) {   
+        if(err){
+            res.status(500).json({ status: "error", message: err});
+        }     
+        res.json(data);
+    });
     // ===============================
 }
 
 function updateProductById(req, res) {
     var payload = req.body
-    var pid = req.params.pid;    
+    var pid = req.params.pid; 
+    var updateproduct = req.body;   
     // #7 Update a product by ID (findByIdAndUpdate)
-
+    Product.findByIdAndUpdate(pid, updateproduct,function(err) {  
+        if(err){
+            res.status(500).json({ status: "Updated a product", message: err});
+        }     
+        res.json(data);
+    });
     // ===============================
 }
 
 function deleteProductById(req, res) {
     var pid = req.params.pid;    
     // #8 Delete a product by ID (findByIdAndDelete)
-
+    Product.findByIdAndDelete(pid,function(err)  {   
+        if(err){
+            res.status(500).json({ status: "Delete a product", message: err});
+        }     
+        res.json(data);
+    });
     // ===============================
 }
 
 function addProduct(req, res) {
     var payload = req.body
     // #9 Add a new product 
-
+    var product = new Product(payload);
+    Product.save(pid,function(err)  {   
+        if(err){
+            res.status(500).json({ status: "Added a product", message: err});
+        }     
+        res.json(data);
+    });
     // ===============================
 }
 
